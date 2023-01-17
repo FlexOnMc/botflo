1a48a7a8c74a43fb4efe6e06d0eb709834a753db
FlexOnMc <95165115+FlexOnMc@users.noreply.github.com>
2023-01-17 12:10:49 +0100

    Update bot2.js

--- bot2.js
+++ bot2.js
@@ -1,13 +1,11 @@
 const mineflayer = require('mineflayer')
 
 const bot = mineflayer.createBot({
-  host: 'donutsmp.net', // minecraft server ip
-  username: 'flow221108@gmail.com', // minecraft username
+  host: 'FlexO_O.aternos.me', // minecraft server ip
+  username: 'BotOmggg', // minecraft username
   // password: 'xxx', // minecraft password, comment out if you want to log into online-mode=false servers
-  port: 25565,                // only set if you need a port that isn't 25565
-  version: '1.18',            // only set if you need a specific version or snapshot (ie: "1.8.9" or "1.16.5"), otherwise it's set automatically
-  auth: 'microsoft'              // only set if you need microsoft auth, then set this to 'microsoft'
-})
+  port: 46045,                // only set if you need a port that isn't 25565
+  version: '1.8.9',            // only set if you need a specific version or snapshot (ie: "1.8.9" or "1.16.5"), otherwise it's set automatically
 
 
 bot.on('kicked', console.log)  //pour voir les erreur de kick
