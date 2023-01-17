9d811c57872f8a5b381249959bec5b00579ae2da
FlexOnMc <95165115+FlexOnMc@users.noreply.github.com>
2023-01-17 12:11:36 +0100

    Update bot2.js

--- bot2.js
+++ bot2.js
@@ -5,7 +5,7 @@ const bot = mineflayer.createBot({
   username: 'BotOmggg', // minecraft username
   // password: 'xxx', // minecraft password, comment out if you want to log into online-mode=false servers
   port: 46045,                // only set if you need a port that isn't 25565
-  version: '1.8.9',            // only set if you need a specific version or snapshot (ie: "1.8.9" or "1.16.5"), otherwise it's set automatically
+  version: '1.8.8',            // only set if you need a specific version or snapshot (ie: "1.8.9" or "1.16.5"), otherwise it's set automatically
 
 
 bot.on('kicked', console.log)  //pour voir les erreur de kick
